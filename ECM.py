fadedffab59ede508590118a87a23b9bd1ce3582
Irch Defluviaire <141326509+nkstat23@users.noreply.github.com>
2025-02-15 15:55:25 +0100

    Update ECM.py

--- ECM.py
+++ ECM.py
@@ -1,10 +1,10 @@
 # Importations nécessaires
 #from jupyter_dash import JupyterDash
-import dash
+from dash import Dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
-import dash_table
+from dash import dash_table
 from dash.dependencies import Input, Output
 import plotly.express as px
 import plotly.graph_objects as go
